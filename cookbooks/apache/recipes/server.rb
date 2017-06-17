5a687a0756ecf130cb14ae991f7c67f6f07794b7
J Jesús Abarca Vargas <jesus@abarca.xyz>
2017-06-17 16:14:04 -0500

    Adds ip address and hostname info to the html file in the apache::server recipe.

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -1,7 +1,7 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1><br/><h2>#{node['ipaddress']}</h2><br/><h2>#{node['hostname']}</h2>"
 end
 
 service 'httpd' do
