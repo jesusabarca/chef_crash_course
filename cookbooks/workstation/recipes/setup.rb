4ac40a2fe901ff100633bedc4bafac0b20e310f2
J Jesús Abarca Vargas <jesus@abarca.xyz>
2017-06-17 16:01:39 -0500

    Expanding the workstation::setup recipe.

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -3,5 +3,13 @@ package 'tree' do
 end
 
 file '/etc/motd' do
-  content 'Sup!'
+  content "Property of ...
+  IPADDRESS: #{node['ipaddress']}
+  HOSTNAME : #{node['hostname']}
+  MEMORY   : #{node['memory']['total']}
+  CPU      : #{node['cpu']['0']['mhz']}
+"
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
